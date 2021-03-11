cbac117632ec0521cf964e71114fb296c0b278db
wicho1001 <wichoguerrero747@gmail.com>
2021-03-10 19:23:25 -0600

    feat: Added tailwind

---
+++ ng-tailwind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: './tailwind.config.js',
+  sourceCSS: './src/tailwind.css',
+  outputCSS: './src/styles.css',
+  watchRelatedFiles: [],
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: ['dynamically-generated-class'], // Problem solved
+  whitelistPatterns: [/dynamically/, /generated/, /class/], // overkill, but also works
+  whitelistPatternsChildren: [],
+  extensions: ['.ts', '.html', '.js'],
+  content: []
+} 
